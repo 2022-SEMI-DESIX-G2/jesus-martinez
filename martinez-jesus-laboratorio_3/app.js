4f58a14b8d65b922ac8c30c76377b3d0caca138d
Jesus Martinez <jesmartinez@bgeneral.com>
2022-04-10 18:18:05 -0500

    save changes

--- martinez-jesus-laboratorio_3/app.js
+++ martinez-jesus-laboratorio_3/app.js
@@ -1,5 +1,15 @@
-function palindromo() {
+function palindromo(n) {
+    num = n.toString()
+    binario = n.toString(2)
 
+    var decimalRev = num.split("").reverse().join("")
+    var BinarioRev = binario.split("").reverse().join("")
+
+    if (num == decimalRev && binario == BinarioRev){
+        console.log("El numero es palíndromo de doble base")
+    }else {
+        console.log("El numero NO es palíndromo de doble base")
+    }
 }
 
 function countWords(w) {
@@ -22,17 +32,35 @@ function bisiestoYear(year) {
 
 function addPrimos(n){
     var num = parseInt(n)
-    for (var i = 0; i < num ; i++ ){
-        if ( i / 2 == 0){
-            console.log(i)
+    for (var i = 2; i < num ; i++ ){
+        if ( num % i == 0){
+            return false
         }
     }
+    return true
 }
 
+function contPrimos(num){
+    n = parseInt(num)
+    sum = 0
+    for (var i = 0; i <= n; i++){
+        if (addPrimos(i)){
+            sum = sum+i
+        }
+    }
+    console.log("La suma de los numero es ",sum)
+}
+
+let numero = 585
 let word = "jeesu 63032966"
 let year = 2000
 let num = 7
 
+console.log("----- Parte 1 ------")
+console.log(palindromo(numero))
+console.log("----- Parte 2 ------")
 console.log(countWords(word))
+console.log("----- Parte 3 ------")
 console.log(bisiestoYear(year))
-console.log(addPrimos(num))
+console.log("----- Parte 4 ------")
+console.log(contPrimos(num))
