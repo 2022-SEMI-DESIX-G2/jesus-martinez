83108c8fea899c28cafa3f077f064b8baa1f5bb4
Jesus Martinez <jesmartinez@bgeneral.com>
2022-07-01 02:38:28 -0500

    save

--- martinez-jesus-proyecto_1/backend/app.js
+++ martinez-jesus-proyecto_1/backend/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 const PORT = process.env.DEVPORT || 3000;
-const TTL = process.env.TTL || 20000;
+const TTL = process.env.TTL || 0;
 const CACHE = {};
 
 app.use(cors());
@@ -38,6 +38,45 @@ app.post("/encounters/:name", async function (req, res) {
     try {
         const { data } = await axios.get(
             `https://pokeapi.co/api/v2/pokemon/${name}/encounters`
+        );
+        responseData = data;
+    } catch (error) {
+        responseData = { error: error.toString(), name };
+    }
+    CACHE[name] = responseData;
+    cacheTime = setTimeout(() => { delete CACHE[name] }, TTL);
+    res.send({ data: responseData, isCached: false, ttl: false });
+});
+
+
+app.post("/species/:name", async function (req, res) {
+    const { name } = req.params;
+    let responseData; 
+    if (CACHE[name]) {
+        return res.json({ data: CACHE[name], isCached: true, ttl: true});
+    }
+    try {
+        const { data } = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon-species/${name}/`
+        );
+        responseData = data;
+    } catch (error) {
+        responseData = { error: error.toString(), name };
+    }
+    CACHE[name] = responseData;
+    cacheTime = setTimeout(() => { delete CACHE[name] }, TTL);
+    res.send({ data: responseData, isCached: false, ttl: false });
+});
+
+app.post("/poekechain/:name", async function (req, res) {
+    const { name } = req.params;
+    let responseData; 
+    if (CACHE[name]) {
+        return res.json({ data: CACHE[name], isCached: true, ttl: true});
+    }
+    try {
+        const { data } = await axios.get(
+            `https://pokeapi.co/api/v2/evolution-chain/${name}/`
         );
         responseData = data;
     } catch (error) {
